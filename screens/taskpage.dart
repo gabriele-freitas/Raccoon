f4a9c32377aad14999bff7da9dcbd5529327784a
Gabriele Freitas <52288192+gabriele-freitas@users.noreply.github.com>
2021-07-11 20:42:31 -0300

    lib

---
+++ screens/taskpage.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+import 'package:todo1/database_helper.dart';
+import 'package:todo1/models/task.dart';
+import 'package:todo1/models/todo.dart';
+import 'package:todo1/widgets.dart';
+
+class Taskpage extends StatefulWidget {
+    final Task task;
+
+    Taskpage({@required this.task});
+
+  @override
+  _TaskpageState createState() => _TaskpageState();
+}
+
+class _TaskpageState extends State<Taskpage> {
+
+  DatabaseHelper _dbHelper = DatabaseHelper();
+
+  int _taskId = 0;
+  String _taskTitle = "";
+  String _taskDescription = "";
+
+  FocusNode _titleFocus;
+  FocusNode _descriptionFocus;
+  FocusNode _todoFocus;
+
+  bool _contentVisile = false;
+
+  @override
+  void initState(){
+    if(widget.task != null){
+      //Set visibility to true
+      _contentVisile = true;
+
+      _taskTitle = widget.task.title;
+      _taskDescription = widget.task.description;
+      _taskId = widget.task.id;
+    }
+
+    _titleFocus = FocusNode();
+    _descriptionFocus = FocusNode();
+    _todoFocus = FocusNode();
+
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    _titleFocus.dispose();
+    _descriptionFocus.dispose();
+    _todoFocus.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Stack(
+            children: [
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 25.0,
+                      bottom: 6.0,
+                    ),
+                    child: Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Image(
+                              image: AssetImage(
+                                'assets/images/voltar.png'
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            focusNode: _titleFocus,
+                            onSubmitted: (value) async {
+                              //Checa se a entrada não está vazia
+                              if(value!= "") {
+                                //Checa se a tarefa/task é nula
+                                if(widget.task == null){
+                                  Task _newtask = Task(title: value);
+                                  _taskId = await _dbHelper.insertTask(_newTask);
+                                  setState(() {
+                                    _contentVisile = true;
+                                    _taskTitle = value;
+                                  });
+                                } else{
+                                  await _dbHelper.updateTaskTitle(_taskId, value);
+                                  print("Task Updated");
+                                }
+                                _descriptionFocus.requestFocus();
+                              }
+                            },
+                            controller: TextEditingController()
+                              ..text = _taskTitle,
+                            decoration: InputDecoration(
+                              hintText: "Insira o título da tarefa",
+                              border: InputBorder.none,
+                            ),
+                            style: TextStyle(
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.bold, //para deixar em negrito a escrita
+                              color: Colors.black26,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                //],
+                //),
+                 Visibility(
+                     visible: _contentVisile,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 12.0,
+                  ),
+                  child: TextField(
+                    focusNode: _descriptionFocus,
+                    onSubmitted: (value) async{
+                      if(value != ""){
+                        if(_taskId != 0){
+                          await _dbHelper.updateTaskDescription(_taskId, value);
+                          _taskDescription = value;
+                        }
+                      }
+                      _todoFocus.requestFocus();
+                    },
+                    controller: TextEditingController()..text = _taskDescription,
+                    decoration: InputDecoration(
+                      hintText: "Insira a descrição da tarefa",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 25.0,
+                      ),
+                    ),
+                    ),
+                  ),
+                 ),
+                    Visibility(
+                      visible: _contentVisile,
+                      child: FutureBuilder(
+                        initialData: [],
+                        future: _dbHelper.getTodo(_taskId),
+                        builder: (context, snapshot) {
+                          return Expanded(
+                            child: ListView.builder(
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (context, index) {
+                                return GestureDetector(
+                                  onTap: () async {
+                                    if(snapshot.data[index].isDone == 0){
+                                      await _dbHelper.updateTodoDone(snapshot.data[index].id, 1);
+                                    } else {
+                                      await _dbHelper.updateTodoDone(snapshot.data[index].id, 0);
+                                    }
+                                    setState(() {});
+                                  },
+                                  child: TodoWidget(
+                                    text: snapshot.data[index].title,
+                                    isDone: snapshot.data[index].isDone == 0
+                                        ? false
+                                        : true,
+                                  ),
+                                );
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Visibility(
+                      visible: _contentVisile,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 24.0,
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 20.0,
+                              height: 20.0,
+                              margin: EdgeInsets.only(
+                                right: 12.0,
+                              ),
+                              decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: BorderRadius.circular(6.0),
+                                  border: Border.all(
+                                      color: Color(0xFF86829D), width: 1.5)),
+                              child: Image(
+                                image: AssetImage('assets/images/check_icon.png'),
+                              ),
+                            ),
+                            Expanded(
+                              child: TextField(
+                                focusNode: _todoFocus,
+                                controller: TextEditingController()..text = "",
+                                onSubmitted: (value) async {
+                                  // Check if the field is not empty
+                                  if (value != "") {
+                                    if (_taskId != 0) {
+                                      DatabaseHelper _dbHelper = DatabaseHelper();
+                                      Todo _newTodo = Todo(
+                                        title: value,
+                                        isDone: 0,
+                                        taskId: _taskId,
+                                      );
+                                      await _dbHelper.insertTodo(_newTodo);
+                                      setState(() {});
+                                      _todoFocus.requestFocus();
+                                    } else {
+                                      print("Task doesn't exist");
+                                    }
+                                  }
+                                },
+                                decoration: InputDecoration(
+                                  hintText: "Enter Todo item...",
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              Visibility(
+                visible: _contentVisile,
+              child: Positioned(
+                bottom: 25.0,
+                right: 25.0,
+                child: GestureDetector(
+                  onTap: () async{
+                    if(_taskId != 0) {
+                      await _dbHelper.deleteTask(_taskId);
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      color: Colors.black26,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Image(
+                      image: AssetImage(
+                          "assets/images/trash_icon.png"
+                      ),
+                    ),
+                    ),
+                  ),
+                ),
+              )
+          ],
+          ),
+        ),
+      ),
+    );
+  }
+}
